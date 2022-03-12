f965e3c3995b831fc708e62426daef29bf505943
the-abe-train <abraham.train@gmail.com>
2022-03-12 15:35:36 -0500

    Introduced framer motion

---
+++ app/components/Instructions.tsx
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation, AnimatePresence } from "framer-motion";
+
+const instructions = [
+  {
+    name: "Play",
+    icon: "./icons/play.svg",
+    text: "Guess the most popular answers to surveys with respondants from around the world.",
+  },
+  {
+    name: "Vote",
+    icon: "./icons/vote.svg",
+    text: "Use the Ballot token to participate in tomorrow’s surveys.",
+  },
+  {
+    name: "Draft",
+    icon: "./icons/draft.svg",
+    text: "Submit custom questions to be a part of upcoming surveys.",
+  },
+];
+
+export default function Instructions() {
+  const [helper, setHelper] = useState(instructions[0]["text"]);
+  const [icon, setIcon] = useState(instructions[0]["icon"]);
+  const [showPopup, setShowPopup] = useState(false);
+  const control = useAnimation();
+
+  const containerVariants = {
+    collapsed: {
+      height: "0px",
+      opacity: 0,
+      transition: {
+        duration: 0.25,
+        when: "afterChildren",
+      },
+    },
+    open: {
+      height: "auto",
+      opacity: 1,
+      transition: {
+        duration: 0.25,
+        when: "beforeChildren",
+      },
+    },
+  };
+
+  const childVariants = {
+    open: {
+      height: "auto",
+      opacity: 1,
+      transition: {
+        duration: 0.25,
+      },
+    },
+    collapsed: {
+      height: "auto",
+      opacity: 0,
+      transition: {
+        duration: 0.25,
+      },
+    },
+  };
+
+  return (
+    <section className="m-4 pb-1 flex-grow">
+      <h1
+        className="text-4xl text-center font-header font-bold flex items-center 
+    w-full justify-center gap-x-2 mt-6"
+        onClick={() => setShowPopup(false)}
+      >
+        <img
+          className="inline h-8 object-fill"
+          src="./icons/logo.svg"
+          alt="logo"
+        />
+        <span>Plurality</span>
+      </h1>
+      <div className="flex w-full justify-around my-4">
+        {instructions.map((instr) => {
+          return (
+            <div
+              key={instr.name}
+              className="flex flex-col items-center pointer"
+              onClick={() => {
+                setHelper(instr.text);
+                setIcon(instr.icon);
+                setShowPopup(true);
+                control.start({
+                  opacity: [1, 0, 1],
+                });
+              }}
+            >
+              <h3 className="text-accent font-header text-2xl">{instr.name}</h3>
+              <div
+                className="flex items-center bg-white sm:border-2 
+        border-black rounded-md p-2 space-x-3"
+              >
+                <img
+                  src={instr.icon}
+                  alt="play"
+                  className="h-9 sm:hidden lg:block"
+                />
+                <p className="hidden sm:block sm:w-44 lg:w-60">{instr.text}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <AnimatePresence initial={false}>
+        {showPopup && (
+          <motion.div
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={containerVariants}
+            className="bg-white border-2 border-black rounded-md sm:hidden"
+          >
+            <motion.div
+              variants={childVariants}
+              // animate={control}
+              className="flex p-2"
+            >
+              <img src={icon} alt="Instruction symbol" className="mr-4" />
+              <p className="m-0">{helper}</p>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </section>
+  );
+}
+
+{
+  /* <div className="bg-white border-2 border-black p-2 rounded-md flex sm:hidden">
+<img src={icon} alt="" className="mr-4" />
+<p>{helper}</p>
+</div> */
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,11 +12,15 @@ import backgrounds from "~/styles/backgrounds.css";
 import animations from "~/styles/animations.css";
 
 import questionData from "~/data/questions.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchPhoto } from "~/util/unsplash";
 import { countAnswers, sumToken } from "~/util/math";
 import { statFormat } from "~/util/text";
 import { timeLeft, relativeSurvey } from "~/util/time";
+
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import Collapse from "~/transitions/Collapse";
+import Instructions from "~/components/Instructions";
 
 export function links() {
   return [
@@ -69,69 +73,13 @@ export default function Index() {
   const data = useLoaderData<IQuestion[]>();
   const [yesterday, today, tomorrow] = data;
 
-  const [helper, setHelper] = useState("");
-  const [icon, setIcon] = useState("");
-
   const closingTime = timeLeft(tomorrow.surveyClosed - new Date().getTime());
 
   return (
     <div className="light w-full top-0 bottom-0 flex flex-col">
       <Header />
       <main className="container">
-        <section className="space-y-3 p-4 flex-grow">
-          <h1
-            className="text-4xl text-center font-header font-bold flex items-center 
-          w-full justify-center gap-x-2"
-            onClick={() => setHelper("")}
-          >
-            <img
-              className="inline h-8 object-fill"
-              src="./icons/logo.svg"
-              alt="logo"
-            />
-            <span>Plurality</span>
-          </h1>
-          <div className="flex w-full justify-around">
-            {instructions.map((instr) => {
-              return (
-                <div
-                  key={instr.name}
-                  className="flex flex-col items-center pointer"
-                  onClick={() => {
-                    setHelper(instr.text);
-                    setIcon(instr.icon);
-                  }}
-                >
-                  <h3 className="text-accent font-header text-2xl">
-                    {instr.name}
-                  </h3>
-                  <div
-                    className="flex items-center bg-white sm:border-2 
-              border-black rounded-md p-2 space-x-3"
-                  >
-                    <img
-                      src={instr.icon}
-                      alt="play"
-                      className="h-9 sm:hidden lg:block"
-                    />
-                    <p className="hidden sm:block sm:w-44 lg:w-60">
-                      {instr.text}
-                    </p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          {helper !== "" && (
-            <div
-              className="bg-white border-2 border-black p-2 rounded-md flex sm:hidden
-            fade-in"
-            >
-              <img src={icon} alt="" className="mr-4" />
-              <p>{helper}</p>
-            </div>
-          )}
-        </section>
+        <Instructions />
         <section
           className="m-4 space-y-4 sm:space-y-0 gap-4 sm:grid"
           style={{
