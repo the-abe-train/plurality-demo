c807e03d58cab136aa1cff7088cb335a5120c757
the-abe-train <abraham.train@gmail.com>
2022-03-15 00:34:57 -0400

    Added favicon and new naem

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,7 @@ export function links() {
 }
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Plurality" };
 };
 
 export default function App() {
@@ -24,6 +24,24 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="favicon/site.webmanifest" />
         <Meta />
         <Links />
       </head>
