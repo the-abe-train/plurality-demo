9ddfd1251810432550e419731a1166d033d2ff64
the-abe-train <abraham.train@gmail.com>
2022-03-15 00:37:51 -0400

    Added search page

---
+++ app/routes/questions/index.tsx
@@ -0,0 +1,197 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  ActionFunction,
+  Form,
+  useActionData,
+  useSubmit,
+  useTransition,
+} from "remix";
+import Question from "~/components/Question";
+import questionData from "~/data/questions.json";
+import { IQuestion } from "~/lib/question";
+import { dateBySurvey } from "~/util/time";
+import { fetchPhoto } from "~/util/unsplash";
+
+import styles from "~/styles/app.css";
+import backgrounds from "~/styles/backgrounds.css";
+import animations from "~/styles/animations.css";
+
+type Metadata = {
+  pageStart: number;
+  pageEnd: number;
+  total: number;
+};
+
+type Data = {
+  results: IQuestion[];
+  metadata: Metadata;
+};
+
+export function links() {
+  return [
+    { rel: "stylesheet", href: styles },
+    { rel: "stylesheet", href: backgrounds },
+    { rel: "stylesheet", href: animations },
+  ];
+}
+
+export const action: ActionFunction = async ({ request }): Promise<Data> => {
+  let body = await request.formData();
+  let textParam = body.get("text");
+  let dateParam = body.get("date");
+  let pageParam = body.get("page");
+
+  const perPage = 6;
+  const pageStart = pageParam ? (Number(pageParam) - 1) * perPage : 0;
+  const pageEnd = pageParam ? Number(pageParam) * perPage : perPage;
+
+  const allQuestions = questionData.filter((q) => {
+    let textQuery, dateQuery;
+    if (textParam) {
+      const re = new RegExp(String.raw`${textParam}`, "g");
+      const textMatch = q.text.match(re);
+      const idMatch = String(q.id) === textParam;
+      textQuery = !!textMatch || idMatch;
+    }
+    if (dateParam) {
+      const dateString = dateBySurvey(q);
+      dateQuery = dateParam === dateString;
+    }
+    return textQuery || dateQuery;
+  });
+
+  const metadata = {
+    total: allQuestions.length,
+    pageStart: Math.min(pageStart, allQuestions.length),
+    pageEnd: Math.min(pageEnd, allQuestions.length),
+  };
+
+  const results = await Promise.all(
+    allQuestions
+      .slice(pageStart, pageEnd)
+      .map(async (question): Promise<IQuestion> => {
+        const photo = await fetchPhoto(question);
+        return { ...question, photo };
+      })
+  );
+  return { results, metadata };
+};
+
+export default function Index() {
+  const submit = useSubmit();
+  const [page, setPage] = useState(1);
+  const formRef = useRef<HTMLFormElement>(null!);
+  const transition = useTransition();
+
+  const data = useActionData<Data>();
+
+  useEffect(() => {
+    if (data?.metadata) {
+      const maxPages = Math.max(Math.ceil(data.metadata.total / 5), 1);
+      setPage(Math.min(page || 1, maxPages));
+    }
+  }, [data]);
+
+  async function turnPage(change: number) {
+    const newFormData = new FormData(formRef.current);
+    const formPage = Number(newFormData.get("page"));
+    let newPage = formPage + change;
+    setPage(newPage);
+    newFormData.set("page", String(newPage));
+    submit(newFormData, {
+      method: "post",
+      action: "/questions?index",
+      replace: true,
+    });
+  }
+
+  return (
+    <div className="container">
+      <Form
+        method="post"
+        className="m-8 flex flex-col space-y-4 max-w-xl mx-auto px-4"
+        ref={formRef}
+        onSubmit={() => console.log(page)}
+      >
+        <h2 className="font-header text-2xl">Search</h2>
+        <input
+          type="text"
+          name="text"
+          placeholder="Search by keyword or question ID"
+          className="border-[1px] border-black px-2"
+        />
+        <input
+          type="date"
+          name="date"
+          id="date"
+          className="border-[1px] border-black flex-grow px-2"
+        />
+        <div className="flex justify-between border-black ">
+          <div className="flex">
+            <button
+              className="px-2 border-[1px] border-black rounded-full"
+              onClick={() => turnPage(-1)}
+              type="button"
+            >
+              -
+            </button>
+            <label className="flex mx-2">
+              Page:{"  "}
+              <input
+                type="text"
+                name="page"
+                id="page"
+                placeholder="Page"
+                className="text-center w-5"
+                onChange={(e) => e.target.value}
+                value={page}
+              />
+            </label>
+            <button
+              className="px-2 border-[1px] border-black rounded-full"
+              onClick={() => turnPage(1)}
+              type="button"
+              disabled={transition.state !== "idle"}
+            >
+              +
+            </button>
+          </div>
+          <div className="space-x-4">
+            <button
+              type="reset"
+              className="text-red-700"
+              onClick={() => setPage(1)}
+              disabled={transition.state !== "idle"}
+            >
+              Reset
+            </button>
+            <button
+              type="submit"
+              className="rounded-sm border-[1px] px-4 border-black"
+              disabled={transition.state !== "idle"}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </Form>
+      <div className="px-4">
+        {data?.metadata?.total && (
+          <section className="my-4">
+            <div className="m-4 flex justify-between">
+              <span>
+                Showing {data.metadata.pageStart + 1} - {data.metadata.pageEnd}{" "}
+                out of {data.metadata.total}
+              </span>
+            </div>
+            <div className="lg:grid lg:grid-cols-3 gap-4">
+              {data.results.map((q: IQuestion) => {
+                return <Question question={q} key={q.id} />;
+              })}
+            </div>
+          </section>
+        )}
+      </div>
+    </div>
+  );
+}
--- app/util/time.ts
+++ app/util/time.ts
@@ -10,9 +10,25 @@ export function relativeSurvey(question: IQuestion, date?: Date) {
   return rd;
 }
 
-export function timeLeft(ms: number) {
+export function timeLeft(surveyClose: number) {
+  const now = new Date().getTime();
   const MS_PER_HOUR = 3_600_000;
-  const hours = ms / MS_PER_HOUR;
+  const MS_PER_DAY = 24 * MS_PER_HOUR;
+  const difference =
+    surveyClose > now ? surveyClose - now : MS_PER_DAY + surveyClose - now;
+  const hours = difference / MS_PER_HOUR;
   const minutes = (hours % 1) * 60;
-  return `${Math.floor(hours)}h ${Math.floor(minutes)}m`;
+  const hoursMinutes = `${Math.floor(hours)}h ${Math.floor(minutes)}m`;
+  if (surveyClose > now) {
+    return `Survey closes in ${hoursMinutes}!`;
+  } else if (surveyClose <= now && difference > 0) {
+    return `Only ${hoursMinutes} left to share your score!`;
+  } else {
+    return "Test your skills on a previous game!";
+  }
+}
+
+export function dateBySurvey(question: IQuestion) {
+  const surveyClose = question.surveyClosed;
+  return new Date(surveyClose).toLocaleDateString("en-CA");
 }
