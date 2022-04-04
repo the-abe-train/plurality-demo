840caee861f21515e6e25d34294b0646b8d59e61
the-abe-train <abraham.train@gmail.com>
2022-04-04 00:01:38 -0400

    Removed unused env variable

--- app/server/env.ts
+++ app/server/env.ts
@@ -1,6 +1,5 @@
 export const MONGO_URL = process.env.MONGO_URL ?? "";
 export const DATABASE_NAME = process.env.DATABASE_NAME ?? "";
-export const JWT_SIGNATURE = process.env.JWT_SIGNATURE ?? "";
 export const COOKIE_SIGNATURE = process.env.COOKIE_SIGNATURE ?? "";
 export const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY ?? "";
 export const FROM_EMAIL = process.env.FROM_EMAIL ?? "";
