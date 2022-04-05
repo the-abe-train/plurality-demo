308978c39d2a98da2dafe8b22401da8877172db4
the-abe-train <abraham.train@gmail.com>
2022-04-04 22:27:53 -0400

    Removed old file

--- app/util/math.ts
+++
@@ -1,9 +0,0 @@
-import { IAnswer, IQuestion } from "~/lib/question";
-
-export function sumToken(answers: IAnswer[]) {
-  return answers.reduce((sum, a) => sum + a.token, 0);
-}
-
-export function countAnswers(question: IQuestion) {
-  return question.answers.length;
-}
