a13e4d53e01a6eb524a98a52805130148f524c02
the-abe-train <abraham.train@gmail.com>
2022-05-10 02:15:10 -0400

    Removed console logs

--- app/routes/verify.$email.$hash.tsx
+++ app/routes/verify.$email.$hash.tsx
@@ -4,9 +4,7 @@ import { client } from "~/db/connect.server";
 import { verifyUser } from "~/db/queries";
 import { createVerifyEmailToken } from "~/util/verify.server";
 
-export const loader: LoaderFunction = async ({ params, request }) => {
-  console.log("Email:", params.email);
-  console.log("hash:", params.hash);
+export const loader: LoaderFunction = async ({ params }) => {
   const { email, hash } = params;
   invariant(email, "Failed to parse email verification string.");
   const verifyToken = await createVerifyEmailToken(email);
